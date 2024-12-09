8515a0739cebef03aa63ccb35141f7aad167cf77
felipem.medeiros <medeirosfe83@gmail.com>
2024-12-09 15:40:45 -0300

    qa

--- src/main/java/medeiros/felipe/lista_tarefas/domain/tarefa/TarefaRepository.java
+++ src/main/java/medeiros/felipe/lista_tarefas/domain/tarefa/TarefaRepository.java
@@ -1,8 +1,6 @@
 package medeiros.felipe.lista_tarefas.domain.tarefa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TarefaRepository extends JpaRepository<Tarefa,Long> {
 }
